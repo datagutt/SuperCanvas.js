13e797346e84e1132392711490ea664dce025e7a
datagutt <datagutt666@hotmail.com>
2012-05-27 14:21:13 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# SuperCanvas.js
+
+This library aims to make using <canvas> easier.
+
+## Browser support
+
+Any browser that supports canvas should work with SuperCanvas. No browser sniffing is used at all.
+
+## License
+
+SuperCanvas is released under the MIT license. Read the LICENSE file for more info.
---
+++ SuperCanvas.js
@@ -0,0 +1,89 @@
+// SuperCanvas.js
+// (c) 2012 datagutt
+// SuperCanvas.js may be freely distributed under the MIT license.
+;(function(doc, global){
+	"use strict";
+	var SuperCanvas = function(canvas, width, height){
+		canvas = this.canvas = canvas;
+		canvas.width = width || "auto";
+		canvas.height = height || "auto";
+		if (canvas.getContext) {
+			this.context = canvas.getContext("2d");
+			this.canUse = true;
+		} else {
+			this.canUse = false;
+		}
+	};
+	
+	SuperCanvas.prototype.font = function(size, font){
+		var context = this.context;
+		context.font = size + " " + font;
+		return context.font;
+	};
+	
+	SuperCanvas.prototype.color = function(color){
+		var context = this.context;
+		context.fillStyle = color;
+		return context.fillStyle;
+	};
+	
+	SuperCanvas.prototype.drawRectangle = function(color, x, y, width, height){
+		var context = this.context;
+		if (color) {	
+			context.fillStyle = color;
+		}
+		context.beginPath();
+		context.fillRect(x, y, width, height);
+		context.closePath();
+		return context.fill();
+	};
+	
+	SuperCanvas.prototype.drawCircle = function(color, x, y, radius){
+		var context = this.context;
+		if (color) {	
+			context.fillStyle = color;
+		}
+		context.beginPath();
+		context.arc(x, y, radius, 0, Math.PI*2, true); 
+		context.closePath();
+		return context.fill();
+	};
+	
+	SuperCanvas.prototype.drawImage = function(url, x, y, width, height){
+		var context = this.context;
+		var image = new Image();
+		image.onload = function(){
+			context.drawImage(image, x, y, width, height);
+		};
+		//image.crossOrigin = "anonymous";
+		image.src = url;
+	};
+	
+	SuperCanvas.prototype.drawText = function(text, x, y, w){
+		var context = this.context;
+		if (w) {
+			return context.fillText(text, x, y, w);
+		} else {
+			return context.fillText(text, x, y);
+		}
+	};
+	
+	SuperCanvas.prototype.clear = function(){
+		var context = this.context;
+		context.clearRect(0, 0, this.canvas.width, this.canvas.height);	
+	};
+	
+	// This was used to be compatible with the regular canvas api
+	// I dont use it anymore because trying to be compatible just caused problems
+	function initAPI(canvas, context){
+		for (var key in context) {
+			var obj = context[key];
+			if (typeof obj == "function") {
+				canvas[key] = obj;
+			}
+		}
+	};
+	
+	global.SuperCanvas = SuperCanvas;
+	
+})(document, window);
